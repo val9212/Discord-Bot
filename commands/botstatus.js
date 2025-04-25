3fa19a225bc03923b86455f75f780e03ea27b0f4
val9212 <val92.hottlet@gmail.com>
2025-04-25 10:03:23 +0200

    Refactor status command for DiscordJS v14

--- commands/botstatus.js
+++ commands/botstatus.js
@@ -1,9 +1,32 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+const { EmbedBuilder } = require('discord.js');
+
 exports.run = (client, message, args) => {
-      const botstatus = ['Online', 'Idle', 'Do Not Disturb', 'Invisable'];
-        const embed = new Discord.RichEmbed()
-        .addField("Bot Status: ", `${botstatus[client.status]}`);
-        message.channel.send(embed)
-}
-   
+    const botstatus = ['Online', 'Idle', 'Do Not Disturb', 'Invisible'];
+    
+    const status = client.user.presence.status;
+    let statusText = '';
+
+    switch (status) {
+        case 'online':
+            statusText = botstatus[0];
+            break;
+        case 'idle':
+            statusText = botstatus[1];
+            break;
+        case 'dnd':
+            statusText = botstatus[2];
+            break;
+        case 'invisible':
+            statusText = botstatus[3];
+            break;
+        default:
+            statusText = 'Unknown';
+    }
+
+    const embed = new EmbedBuilder()
+        .addFields(
+            { name: "Bot Status", value: statusText }
+        );
+
+    message.channel.send({ embeds: [embed] });
+};
