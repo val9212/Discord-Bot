af2eb8b8ef64578081101dccf94bc6f1078472f3
val9212 <val92.hottlet@gmail.com>
2025-04-25 20:34:42 +0200

    Refactor exec command to improve structure and readability

--- commands/exec.js
+++ commands/exec.js
@@ -1,30 +1,41 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+const { EmbedBuilder } = require('discord.js');
 const { exec } = require('child_process');
-exports.run = async (client, message, args) => {
-        if(message.author.id !== "352899523158474752") return message.channel.send("Only bot owner can use this command")
+const config = require("../config.js")
 
-        const code = args.join(' ');
-		if (!code) return message.channel.send('You provided no input are you stupid?');
-		exec(code, (error, stdout, stderr) => {
-			const input = `\`\`\`Bash\n${code}\n\`\`\``;
-			if (error) {
-				let output = `\`\`\`Bash\n${error}\n\`\`\``;
-				const embed = new Discord.RichEmbed()
-					.setTitle('Execute')
-					.addField(':inbox_tray: Input', input)
-					.addField(':x: Error', output)
-					.setColor(0x00A2E8)
-				return message.channel.send(embed);
-			} else {
-				const output = stderr || stdout;
-				const output2 = `\`\`\`Bash\n${output}\n\`\`\``;
-				const embed = new Discord.RichEmbed()
-					.setTitle('Execute')
-					.addField(':inbox_tray: Input', input)
-					.addField(':outbox_tray: Output', output2)
-					.setColor(0x00A2E8)
-				return message.channel.send(embed);
-			}
-		});
-}
+
+exports.run = async (client, message, args) => {
+    if (message.author.id !== config.OWNER_ID) {
+        return message.channel.send("Only the bot owner can use this command.");
+    }
+
+    const code = args.join(' ');
+    if (!code) return message.channel.send("You provided no input, are you sure you're not just trolling?");
+
+    exec(code, (error, stdout, stderr) => {
+        const input = `\`\`\`Bash\n${code}\n\`\`\``;
+
+        if (error) {
+            const errorOutput = `\`\`\`Bash\n${error.message}\n\`\`\``;
+            const errorEmbed = new EmbedBuilder()
+                .setTitle("Execution Error")
+                .addFields(
+                    { name: ":inbox_tray: Input", value: input },
+                    { name: ":x: Error", value: errorOutput }
+                )
+                .setColor(0xFF0000);
+
+            return message.channel.send({ embeds: [errorEmbed] });
+        }
+
+        const output = stderr || stdout;
+        const outputEmbed = new EmbedBuilder()
+            .setTitle("Execution Result")
+            .addFields(
+                { name: ":inbox_tray: Input", value: input },
+                { name: ":outbox_tray: Output", value: `\`\`\`Bash\n${output}\n\`\`\`` }
+            )
+            .setColor(0x00A2E8);
+
+        message.channel.send({ embeds: [outputEmbed] });
+    });
+};
