d5c5c3c13de75eb5655688a7b566cda53036ff36
val9212 <val92.hottlet@gmail.com>
2025-04-25 09:55:57 +0200

    Update choose command for DiscordJS v14

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,13 +1,15 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+const { Client, EmbedBuilder } = require("discord.js");
+
 exports.run = (client, message, args) => {
-      const embed = new Discord.RichEmbed()
-      .setAuthor(client.user.username, client.user.avatarURL)
-      .setColor(0x00A2E8)
-      .setTitle("Changelog v0.9.4")
-      .addField("Changes", "- Added meme\n- Added showerthoughts\n- Added choose \n- Improvements to warnings system\n- Fixed other small bugs")
-      .setTimestamp()
-      .setFooter(client.user.username, client.user.avatarURL);
-      message.channel.send({embed}) 
- }
-   
+    const embed = new EmbedBuilder()
+        .setAuthor({ name: client.user.username, iconURL: client.user.avatarURL() })
+        .setColor(0x00A2E8)
+        .setTitle("Changelog v1.0.5")
+        .addFields(
+            { name: "Changes", value: "- UPDATE FOR DISCORDJS V14" }
+        )
+        .setTimestamp()
+        .setFooter({ text: client.user.username, iconURL: client.user.avatarURL() });
+
+    message.channel.send({ embeds: [embed] });
+};
