f54beaf77ae4216f9457188013b9764ed3b00916
val9212 <val92.hottlet@gmail.com>
2025-04-25 10:04:31 +0200

    Refactor buttslap command for DiscordJS v14

--- commands/buttslap.js
+++ commands/buttslap.js
@@ -1,32 +1,67 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const snek = require('snekfetch');
-const fsn = require('fs-nextra');
+const { Client, PermissionsBitField, AttachmentBuilder } = require('discord.js');
+const fetch = require('node-fetch');
+const fs = require('fs/promises');  // Utilisation de fs/promises
+const { createCanvas, loadImage } = require('canvas');
+
 exports.run = async (client, message, args) => {
-  if (!message.guild.member(client.user).hasPermission('ATTACH_FILES')) return message.reply('Sorry, i dont have the perms to do this cmd i need ATTACH_FILES. :x:')
-const { Canvas } = require('canvas-constructor');
-   if (message.mentions.users.size < 1) return message.channel.send("You didn't mention a user to slap");
-   const getSlapped = async (slapper, slapped) => {
-    const plate = await fsn.readFile('./assets/images/plate_buttslap.png');
+  if (!message.guild.members.me.permissions.has(PermissionsBitField.Flags.AttachFiles)) {
+    return message.reply('Sorry, I need the `ATTACH_FILES` permission to run this command. :x:');
+  }
+
+  if (message.mentions.users.size < 1) {
+    return message.channel.send("You didn't mention a user to slap.");
+  }
+
+  const getSlapped = async (slapper, slapped) => {
+    const plate = await fs.readFile('./assets/images/plate_buttslap.png');  // Utilisation de fs.promises.readFile
     const pngSlapper = slapper.replace('.gif', '.png');
     const pngSlapped = slapped.replace('.gif', '.png');
-    const Slapper = await snek.get(pngSlapper);
-    const Slapped = await snek.get(pngSlapped);
-    return new Canvas(900, 475)
-      .addImage(plate, 0, 0, 850, 475)
-      .addImage(Slapper.body, 252, -39, 131, 131, { type: 'round', radius: 66 })
-      .restore()
-      .addImage(Slapped.body, 460, 167, 131, 131, { type: 'round', radius: 66 })
-      .restore()
-      .toBuffer();
-  }
+    
+    const Slapper = await fetch(pngSlapper);
+    const Slapped = await fetch(pngSlapped);
+
+    const slapperBuffer = await Slapper.buffer();
+    const slappedBuffer = await Slapped.buffer();
+
+    const canvas = createCanvas(900, 475);
+    const ctx = canvas.getContext('2d');
+
+    const plateImage = await loadImage(plate);
+    ctx.drawImage(plateImage, 0, 0, 850, 475);
+
+    const slapperImage = await loadImage(slapperBuffer);
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(252 + 65, -39 + 65, 66, 0, Math.PI * 2);
+    ctx.clip();
+    ctx.drawImage(slapperImage, 252, -39, 131, 131);
+    ctx.restore();
+
+    const slappedImage = await loadImage(slappedBuffer);
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(460 + 65, 167 + 65, 66, 0, Math.PI * 2);
+    ctx.clip();
+    ctx.drawImage(slappedImage, 460, 167, 131, 131);
+    ctx.restore();
+
+    return canvas.toBuffer();
+  };
+
   try {
-    const slapped = message.mentions.users.first().avatarURL;
-    const slapper = message.author.avatarURL;
+    const slapped = message.mentions.users.first().displayAvatarURL({ extension: 'png', size: 512 });
+    const slapper = message.author.displayAvatarURL({ extension: 'png', size: 512 });
+
+    if (!message.channel.nsfw) {
+      return message.channel.send("You need to be in an NSFW channel to use this command.");
+    }
+
     const result = await getSlapped(slapper, slapped);
-    if (!message.channel.nsfw) return message.channel.send("Need to be in a nsfw channel to use this command.")
-    await message.channel.send({ files: [{ attachment: result, name: 'buttslapped.png' }] }); 
+
+    const attachment = new AttachmentBuilder(result, { name: 'buttslapped.png' });
+    await message.channel.send({ files: [attachment] });
   } catch (error) {
-    throw error;
+    console.error(error);
+    message.reply("An error occurred while generating the image. :x:");
   }
-}
+};
