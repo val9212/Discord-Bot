41d3180f40117fcb8b5ac14e2fdb447728b5bb45
val9212 <val92.hottlet@gmail.com>
2025-04-25 10:02:17 +0200

    Refactor Cleverbot command and improve error handling

--- commands/cb.js
+++ commands/cb.js
@@ -1,19 +1,27 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+const { Client, PermissionsBitField } = require("discord.js");
 const Cleverbot = require('cleverio');
-        const clevs = new Cleverbot({
-            key: "KEY",
-            user: "USERNAME",
-            nick: "Mister UwU"
-        });
+const config = require('../config.js')
+const clevs = new Cleverbot({
+    key: config.CLEVERBOT_KEY,  
+    user: config.CLEVERBOT_USER,  
+    nick: config.CLEVERBOT_NICK
+});
+
+clevs.create();
+
 exports.run = async (client, message, args) => {
     try {
-        clevs.create();
-        const text = args.join(" ")
+        if (args.length < 1) {
+            return message.channel.send("Please provide some text for me to respond to! :thinking:");
+        }
+
+        const text = args.join(" ");
         const { response } = await clevs.ask(text);
+
         message.channel.send(response);
     } catch (err) {
-        console.log(err)
+        console.log(err);
+        message.channel.send("Oops! Something went wrong while trying to talk to Cleverbot. Please try again later.");
     }
-}
-   
+};
+
