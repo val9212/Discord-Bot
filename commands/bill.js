b0d290bc38388009307b9fb6ccb667bd7c5bf6e3
val9212 <val92.hottlet@gmail.com>
2025-04-25 10:04:12 +0200

    Refactor bill command for DiscordJS v14

--- commands/bill.js
+++ commands/bill.js
@@ -1,26 +1,42 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const snek = require('snekfetch');
-const fsn = require('fs-nextra');
+const { AttachmentBuilder, PermissionsBitField } = require('discord.js');
+const { createCanvas, loadImage } = require('canvas');
+const fs = require('fs/promises');
+const fetch = require('node-fetch');
+
 exports.run = async (client, message, args) => {
-  if (!message.guild.member(client.user).hasPermission('ATTACH_FILES')) return message.reply('Sorry, i dont have the perms to do this cmd i need ATTACH_FILES. :x:')
-   const { Canvas } = require('canvas-constructor');
-    if (message.mentions.users.size < 1) return message.channel.send("You didn't mention a user to make them bill");
-   const getSlapped = async (person) => {
-    const plate = await fsn.readFile('./assets/images/plate_bill.png');
-    const png = person.replace('.gif', '.png');
-    const { body } = await snek.get(png);
-    return new Canvas(325, 150)
-    .setColor(0x00A2E8)
-    .addImage(body, 80, 0, 150, 150)
-    .addImage(plate, 0, 0, 325, 150)
-    .toBuffer();
+  if (!message.guild.members.me.permissions.has(PermissionsBitField.Flags.AttachFiles)) {
+    return message.reply("Sorry, I need the `ATTACH_FILES` permission to run this command. :x:");
   }
-     try {
-    const person = message.mentions.users.first().avatarURL;
-    const result = await getSlapped(person);
-    await message.channel.send({ files: [{ attachment: result, name: 'bill.png' }] });
+
+  const user = message.mentions.users.first();
+  if (!user) return message.channel.send("You didn't mention a user to make them bill!");
+
+  const getBillImage = async (avatarURL) => {
+    const plate = await fs.readFile('./assets/images/plate_bill.png');
+    const avatarURLPng = avatarURL.replace('.gif', '.png') + '?size=512';
+    const response = await fetch(avatarURLPng);
+    const buffer = await response.arrayBuffer();
+    const avatar = await loadImage(Buffer.from(buffer));
+    const plateImage = await loadImage(plate);
+
+    const canvas = createCanvas(325, 150);
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = '#00A2E8';
+    ctx.fillRect(0, 0, 325, 150);
+    ctx.drawImage(avatar, 80, 0, 150, 150);
+    ctx.drawImage(plateImage, 0, 0, 325, 150);
+
+    return canvas.toBuffer();
+  };
+
+  try {
+    const avatarURL = user.displayAvatarURL({ extension: 'png', size: 512 });
+    const result = await getBillImage(avatarURL);
+    const attachment = new AttachmentBuilder(result, { name: 'bill.png' });
+    await message.channel.send({ files: [attachment] });
   } catch (error) {
-    throw error;
+    console.error(error);
+    message.reply("An error occurred while generating the image. :x:");
   }
-}
+};
