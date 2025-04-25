b0256c3b6aa7c5cd11ac7fc2c64a8d7d23472ec5
val9212 <val92.hottlet@gmail.com>
2025-04-25 20:34:03 +0200

    Refactor command for discordJS v14

--- commands/e926.js
+++ commands/e926.js
@@ -1,80 +1,55 @@
-const Discord = require("discord.js");
-const request = require("request")
-const bot = new Discord.Client();
+const { EmbedBuilder } = require('discord.js');
+const fetch = require('node-fetch');
+const config = require("../config.js")
+
 exports.run = async (client, message, args) => {
-        var estoBanList = [
-            "9/11",
-            "swastika",
-            "kids",
-            "jew",
-            "hitler",
-            "murder",
-            "suicidal",
-            "suicide",
-            "dead",
-            "retard",
-            "gore",
-            "retarded",
-            "cancer",
-            "cancerous",
-            "scat",
-            "shit",
-            "crap",
-            "poo",
-            "pee",
-            "feces",
-            "urin",
-            "piss",
-            "diaper",
-            "baby",
-            "babies",
-            "defecation",
-            "child",
-            "kid",
-            "young",
-            "tod",
-            "cub",
-            "loli",
-            "underaged",
-            "toddler",
-            "cake_farts",
-            "diarrhea",
-            "soiled",
-            "snuff",
-            "watersports",
-            "unbirthing",
-            "hyper",
-            "expansion",
-            "inflation",
-            "guro",
-            "nightmare_fuel"
-        ]
-        var tagesto = "";
-        var args = message.content.split(" ").slice(1);
-        var tagestosplit = args.join(" ")
-        console.log(`NSFW ${tagestosplit} NSFW`);
-            if(estoBanList.indexOf(tagestosplit) != -1){
-                message.channel.send("banned tag :thinking: ?!");
-                return;
-            }
 
-            tagesto = tagestosplit
 
-            var estoHeader = {
-                url: 'https://e926.net/post/index.json?tags=order:random+' + tagestosplit,
-                headers: {
-                    'User-Agent': 'bot pineapple'
+  const bannedTags = [
+    "shota", "age_difference", "stocking", "9/11", "swastika", "kids", "jew",
+    "hitler", "murder", "suicidal", "suicide", "dead", "retard", "gore",
+    "retarded", "cancer", "cancerous", "scat", "shit", "crap", "poo", "pee",
+    "feces", "urin", "piss", "diaper", "baby", "babies", "defecation", "child",
+    "kid", "young", "tod", "cub", "loli", "underaged", "toddler", "cake_farts",
+    "diarrhea", "soiled", "snuff", "watersports", "unbirthing", "hyper",
+    "expansion", "inflation", "guro", "nightmare_fuel"
+  ];
 
-                }
-            }
+  const tags = args.join(' ').toLowerCase();
 
-            request(estoHeader,
-            function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var estoThing = JSON.parse(body);
-                    if (typeof (estoThing[0]) != "undefined") {
-                        message.channel.send(estoThing[0].file_url.toString().replace('https', 'http'))
-                        console.log(`SFW ${estoThing[0].file_url} SFW`);
-                        }else{
-                            message.channel.send("No pictures found. *try another tag*")
-                        }}})}
+  if (bannedTags.some(tag => tags.includes(tag))) {
+    return message.channel.send("Banned tag detected. :thinking:");
+  }
+
+  try {
+    const response = await fetch(`https://e926.net/posts.json?tags=order:random+${encodeURIComponent(tags)}+-young+-child+-cub+-loli+-shota`, {
+      headers: {
+        'User-Agent': `${config.E621_USER}`
+      }
+    });
+
+    if (!response.ok) {
+      return message.channel.send("Failed to fetch data from e621.");
+    }
+
+    const data = await response.json();
+
+    if (!data.posts || data.posts.length === 0) {
+      return message.channel.send("No results found. Try different tags.");
+    }
+
+    const post = data.posts[Math.floor(Math.random() * data.posts.length)];
+
+    const embed = new EmbedBuilder()
+      .setTitle(`Post #${post.id}`)
+      .setURL(`https://e621.net/posts/${post.id}`)
+      .setImage(post.file.url)
+      .setColor(0x00A2E8)
+      .setFooter({ text: `Tags: ${post.tags.general.slice(0, 5).join(', ')}...` });
+
+    await message.channel.send({ embeds: [embed] });
+  } catch (error) {
+    console.error(error);
+    message.channel.send("An error occurred while fetching data from e621.");
+  }
+};
