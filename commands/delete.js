549bbf6099c70ae7042d81bf66fe46c9eecc2f7e
val9212 <val92.hottlet@gmail.com>
2025-04-25 16:58:31 +0200

    Refactor delete command to use updated Discord.js

--- commands/delete.js
+++ commands/delete.js
@@ -1,27 +1,37 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const snek = require('snekfetch');
-const fsn = require('fs-nextra');
+const { AttachmentBuilder, PermissionsBitField } = require('discord.js');
+const fs = require('fs/promises');
+const { createCanvas, loadImage } = require('canvas');
+
 exports.run = async (client, message, args) => {
-  if (!message.guild.member(client.user).hasPermission('ATTACH_FILES')) return message.reply('Sorry, i dont have the perms to do this cmd i need ATTACH_FILES. :x:')
-   const { Canvas } = require('canvas-constructor');
-    if (message.mentions.users.size < 1) return message.channel.send("You didn't mention a user to delete them");
-   const getSlapped = async (person) => {
-    const plate = await fsn.readFile('./assets/images/plate_delete.png');
-    const png = person.replace('.gif', '.png');
-    const { body } = await snek.get(png);
-    return new Canvas(550, 275)
-    .setColor(0x00A2E8)
-    .addRect(0, 0, 634, 675)
-    .addImage(plate, 0, 0, 550, 275)
-    .addImage(body, 92, 106, 139, 151)
-    .toBuffer();
+  if (!message.guild.members.me.permissions.has(PermissionsBitField.Flags.AttachFiles)) {
+    return message.reply("Sorry, I don't have the permissions to do this command. I need ATTACH_FILES. :x:");
   }
-     try {
-    const person = message.mentions.users.first().avatarURL;
-    const result = await getSlapped(person);
-    await message.channel.send({ files: [{ attachment: result, name: 'garbagememe.png' }] });
+
+  if (message.mentions.users.size < 1) {
+    return message.channel.send("You didn't mention a user to apply the effect!");
+  }
+
+  const user = message.mentions.users.first();
+  try {
+    const avatarURL = user.displayAvatarURL({ extension: 'png', size: 256 });
+    const [avatar, plate] = await Promise.all([
+      loadImage(avatarURL),
+      loadImage('./assets/images/plate_delete.png')
+    ]);
+
+    const canvas = createCanvas(550, 275);
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(plate, 0, 0, 550, 275);
+    ctx.drawImage(avatar, 92, 106, 139, 151);
+
+    const buffer = canvas.toBuffer('image/png');
+    const attachment = new AttachmentBuilder(buffer, { name: 'garbagememe.png' });
+
+    await message.channel.send({ files: [attachment] });
+
   } catch (error) {
-    throw error;
+    console.error(error);
+    await message.channel.send(":x: Something went wrong while generating the image.");
   }
-}
+};
