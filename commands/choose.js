8189aaa22fd2b52941a5720c958c6c133715fa27
val9212 <val92.hottlet@gmail.com>
2025-04-25 09:56:24 +0200

    Update choose command for DiscordJS v14

--- commands/choose.js
+++ commands/choose.js
@@ -1,9 +1,15 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+const { EmbedBuilder } = require('discord.js');
+
 exports.run = (client, message, args) => {
-  const choice1 = args[0]
-  const choice2 = args.slice(1).join(" ")
-  if (choice2 < 1) return message.channel.send("Didnt provide a second option to choose from.")
-  var choices = [`${choice1}`, `${choice2}`]
-  message.channel.send(`I choose ${choices[Math.floor(Math.random() * choices.length)]}!`);
-}
+    const choice1 = args[0];
+    const choice2 = args.slice(1).join(" ");
+
+    if (!choice1 || !choice2) {
+        return message.channel.send("You need to provide two options to choose from.");
+    }
+
+    const choices = [choice1, choice2];
+    const selected = choices[Math.floor(Math.random() * choices.length)];
+
+    message.channel.send(`I choose **${selected}**!`);
+};
