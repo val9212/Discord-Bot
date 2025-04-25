3d51d2290bd2789d2faecb0bbb28be8a34454fdc
val9212 <val92.hottlet@gmail.com>
2025-04-25 16:59:27 +0200

    Refactor disabledshrug command for discordJS v14

--- commands/disabledshrug.js
+++ commands/disabledshrug.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+const { Client } = require('discord.js');
+
 exports.run = async (client, message, args) => {
-    message.channel.send("¯\_(ツ)_/¯");
-}
+    message.channel.send("¯\\_(ツ)_/¯");
+};
