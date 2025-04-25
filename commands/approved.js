f4cb3106b62cb52f10872f55787b96d2f1387f99
val9212 <val92.hottlet@gmail.com>
2025-04-25 09:49:37 +0200

    Refactor `approved.js` for DiscordJS v14

--- commands/approved.js
+++ commands/approved.js
@@ -1,26 +1,37 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const snek = require('snekfetch');
-const fsn = require('fs-nextra');
+const { AttachmentBuilder, PermissionsBitField } = require('discord.js');
+const fs = require('fs/promises');
+const { createCanvas, loadImage } = require('canvas');
+
 exports.run = async (client, message, args) => {
-  if (!message.guild.member(client.user).hasPermission('ATTACH_FILES')) return message.reply('Sorry, i dont have the perms to do this cmd i need ATTACH_FILES. :x:')
-   const { Canvas } = require('canvas-constructor');
-    if (message.mentions.users.size < 1) return message.channel.send("You didn't mention a user to put them behind bars");
-   const getSlapped = async (person) => {
-    const plate = await fsn.readFile('./assets/images/approved.png');
-    const png = person.replace('.gif', '.png');
-    const { body } = await snek.get(png);
-    return new Canvas(250, 250)
-    .resetTransformation()
-    .addImage(body, 0, 0, 250, 250)
-    .addImage(plate, 0, 0, 250, 250)
-    .toBuffer();
+  if (!message.guild.members.me.permissions.has(PermissionsBitField.Flags.AttachFiles)) {
+    return message.reply("Sorry, I don't have the permissions to do this command. I need ATTACH_FILES. :x:");
   }
-     try {
-    const person = message.mentions.users.first().avatarURL;
-    const result = await getSlapped(person);
-    await message.channel.send({ files: [{ attachment: result, name: 'approved.png' }] });
+
+  const user = message.mentions.users.first();
+  if (!user) {
+    return message.channel.send("You didn't mention a user to put them behind the approved stamp!");
+  }
+
+  try {
+    const avatarURL = user.displayAvatarURL({ extension: 'png', size: 256 });
+    const [avatar, stamp] = await Promise.all([
+      loadImage(avatarURL),
+      loadImage('./assets/images/approved.png')
+    ]);
+
+    const canvas = createCanvas(250, 250);
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(avatar, 0, 0, 250, 250);
+    ctx.drawImage(stamp, 0, 0, 250, 250);
+
+    const buffer = canvas.toBuffer('image/png');
+    const attachment = new AttachmentBuilder(buffer, { name: 'approved.png' });
+
+    await message.channel.send({ files: [attachment] });
+
   } catch (error) {
-    throw error;
+    console.error(error);
+    await message.channel.send(":x: Something went wrong while generating the image.");
   }
-}
+};
