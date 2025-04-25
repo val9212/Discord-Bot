1454af4adc1f2b56fb0e733808050bd8b9161469
val9212 <val92.hottlet@gmail.com>
2025-04-25 20:34:25 +0200

    Refactor eval command to improve security and code clarity

--- commands/eval.js
+++ commands/eval.js
@@ -1,32 +1,53 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-exports.run = async (client, message, args) => {
-        if(message.author.id !== "352899523158474752") return message.channel.send("Only bot owner can use this command")
-    try {
-      var code = args.join(" ");
-      if (code === "client.token") return message.channel.send("Dont wanna do that 0_0")
-      var evaled = eval(code);
+const { EmbedBuilder } = require('discord.js');
+const util = require('util');
+const config = require("../config.js")
 
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-      
-      const embed = new Discord.RichEmbed()
-        .setColor(0x00A2E8)
-        .addField(":inbox_tray: Input: ", `\`\`\`${code}\`\`\``)
-        .addField(":outbox_tray: output: ", `\`\`\`js\n${clean(evaled)}\n\`\`\``)
-      message.channel.send({embed})
-    } catch (err) {
-      const embed = new Discord.RichEmbed()
-      .setColor(0x00A2E8)
-      .addField(":inbox_tray: Input: ", `\`\`\`${code}\`\`\``)
-      .addField(":outbox_tray: output: ", `\`\`\`${clean(err)}\`\`\``)
-    message.channel.send({embed})
+exports.run = async (client, message, args) => {
+    if (message.author.id !== config.OWNER_ID) {
+        return message.channel.send("Only the bot owner can use this command.");
     }
 
-function clean(text) {
-  if (typeof(text) === 'string')
-    return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
-  else
-      return text;
-  }
-}
+    const code = args.join(" ");
+    if (!code) return message.channel.send("Please provide some code to evaluate.");
+
+    if (code.includes("client.token")) {
+        return message.channel.send("Nice try, but I won't reveal my token! 😶");
+    }
+
+    try {
+        let evaled = eval(code);
+        if (typeof evaled !== "string") {
+            evaled = util.inspect(evaled, { depth: 0 });
+        }
+
+        const outputEmbed = new EmbedBuilder()
+            .setColor(0x00A2E8)
+            .addFields(
+                { name: ":inbox_tray: Input", value: `\`\`\`js\n${code}\n\`\`\`` },
+                { name: ":outbox_tray: Output", value: `\`\`\`js\n${clean(evaled)}\n\`\`\`` }
+            );
+
+        message.channel.send({ embeds: [outputEmbed] });
+
+    } catch (err) {
+        const errorEmbed = new EmbedBuilder()
+            .setColor(0xFF0000)
+            .addFields(
+                { name: ":inbox_tray: Input", value: `\`\`\`js\n${code}\n\`\`\`` },
+                { name: ":x: Error", value: `\`\`\`js\n${clean(err.message)}\n\`\`\`` }
+            );
+
+        message.channel.send({ embeds: [errorEmbed] });
+    }
+
+    function clean(text) {
+        if (typeof text === "string") {
+            return text
+                .replace(/`/g, "`\u200b")
+                .replace(/@/g, "@\u200b")
+                .replace(client.token, "[TOKEN REDACTED]");
+        } else {
+            return text;
+        }
+    }
+};
