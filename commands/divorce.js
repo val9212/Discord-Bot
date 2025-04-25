175c78fee69909407b73f7cea8a703e899669465
val9212 <val92.hottlet@gmail.com>
2025-04-25 16:59:41 +0200

    Refactor divorce command to use updated Discord.js

--- commands/divorce.js
+++ commands/divorce.js
@@ -1,15 +1,15 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const snekfetch = require("snekfetch")
+const { EmbedBuilder } = require('discord.js');
+
 exports.run = async (client, message, args) => {
-      if (message.mentions.users.size < 1) return message.channel.send("you can't divorce nobody")
-      let user = message.guild.member(message.mentions.users.first());
-            message.channel.send(`${user} You got divorced with ${message.author.username} :broken_heart:`,{
-                embed: {
-                    image: {
-                        url: "https://i.imgur.com/IgvLWaa.gif"
-                    }
-                }
-            })
-}
-   
+    if (message.mentions.users.size < 1) {
+        return message.channel.send("You can't divorce nobody.");
+    }
+
+    let user = message.guild.members.cache.get(message.mentions.users.first().id);
+
+    const embed = new EmbedBuilder()
+        .setDescription(`${user} You got divorced with ${message.author.username} :broken_heart:`)
+        .setImage("https://i.imgur.com/IgvLWaa.gif");
+
+    message.channel.send({ embeds: [embed] });
+};
