2fac2376befbd4340eb9b4fc6fdf68461dad4251
val9212 <val92.hottlet@gmail.com>
2025-04-25 20:35:14 +0200

    Refactor fist-bump command to use updated Discord.js features

--- commands/fist-bump.js
+++ commands/fist-bump.js
@@ -1,15 +1,14 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const snekfetch = require("snekfetch")
+const { EmbedBuilder } = require('discord.js');
+
 exports.run = async (client, message, args) => {
-      if (message.mentions.users.size < 1) return message.channel.send("you can't fist-bump nobody")
-      let user = message.guild.member(message.mentions.users.first());
-            message.channel.send(`${user} You got a fist-bump from ${message.author.username} ❤`,{
-                embed: {
-                    image: {
-                        url: "https://i.imgur.com/lO2xZHC.gif"
-                    }
-                }
-            })
-}
-   
+    if (message.mentions.users.size < 1) return message.channel.send("You can't fist-bump nobody.");
+
+    const user = message.guild.members.cache.get(message.mentions.users.first().id);
+
+    const embed = new EmbedBuilder()
+        .setColor(0x00A2E8)
+        .setDescription(`${user} You got a fist-bump from ${message.author.username} ❤`)
+        .setImage('https://i.imgur.com/lO2xZHC.gif');
+
+    message.channel.send({ embeds: [embed] });
+};
