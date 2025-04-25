349445a376c5a3d6d1878848abe5234e8b417877
val9212 <val92.hottlet@gmail.com>
2025-04-25 09:53:36 +0200

    Update awards command for DiscordJS v14

--- commands/awards.js
+++ commands/awards.js
@@ -1,22 +1,27 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+const { EmbedBuilder } = require("discord.js");
+
 exports.run = (client, message, args) => {
-    const embed2 = new Discord.RichEmbed()
-    .setColor(0x00A2E8) 
-    .setTitle(" Possible Awards ")
-    .addField(`:tada: - Reach level 25`, ` + 5,000 money`, true)
-    .addField(`:medal: - Reach level 50`, `+ 10,000 money`, true)
-    .addField(`:military_medal: - Reach level 75`, `+ 20,000 money`, true)
-    .addField(`:trophy:`, `Reach level 100 (max)`, true)
-    .addBlankField()
-    .addField(`:first_place: - Reach #1 on the leaderboard`, `+5% winning chance`, false)
-    .addField(`:moneybag:`, `Earn 10,000 in cash. +1% winning chance`, true)
-    .addField(`:credit_card:`, `Earn 100,000 in cash. +3% winning chance`, true)
-    .addBlankField()
-    .addField(`:spy::skin-tone-1:`, `Secret Award`, true)
-    .addField(`:tophat: - Secret Award`, ` +`, true)
-    .addField(`:watch:`, `Secret Award`, true)
-    .addField(`:tools:`, `Secret Award`, true)
-    message.channel.send(embed2) 
-}
-   
+  const embed = new EmbedBuilder()
+    .setColor(0x00A2E8)
+    .setTitle("🏆 Possible Awards")
+    .addFields(
+      { name: `:tada: - Reach level 25`, value: `+ 5,000 money`, inline: true },
+      { name: `:medal: - Reach level 50`, value: `+ 10,000 money`, inline: true },
+      { name: `:military_medal: - Reach level 75`, value: `+ 20,000 money`, inline: true },
+      { name: `:trophy:`  , value: `Reach level 100 (max)`, inline: true },
+      { name: '\u200B', value: '\u200B' }, // Blank field
+
+      { name: `:first_place: - Reach #1 on the leaderboard`, value: `+5% winning chance`, inline: false },
+      { name: `:moneybag:`, value: `Earn 10,000 in cash. +1% winning chance`, inline: true },
+      { name: `:credit_card:`, value: `Earn 100,000 in cash. +3% winning chance`, inline: true },
+      { name: '\u200B', value: '\u200B' }, // Blank field
+
+      { name: `:spy::skin-tone-1:`, value: `Secret Award`, inline: true },
+      { name: `:tophat: - Secret Award`, value: `+`, inline: true },
+      { name: `:watch:`, value: `Secret Award`, inline: true },
+      { name: `:tools:`, value: `Secret Award`, inline: true },
+    );
+
+  message.channel.send({ embeds: [embed] });
+};
+
