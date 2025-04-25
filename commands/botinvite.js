c514b53efc169b9ebb145fb3dc0e5cd1ef90392e
val9212 <val92.hottlet@gmail.com>
2025-04-25 09:54:36 +0200

    Update botinvite command for DiscordJS v14

--- commands/botinvite.js
+++ commands/botinvite.js
@@ -1,5 +1,3 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-exports.run = (client, message, args) => {
-return message.channel.send("here is a link to invite the bot: https://discordapp.com/oauth2/authorize?client_id=618011652448845854&permissions=8&scope=bot")
-}
+exports.run = (client, message) => {
+    return message.channel.send("Here is a link to invite the bot: https://discordapp.com/oauth2/authorize?client_id=618011652448845854&permissions=8&scope=bot");
+};
