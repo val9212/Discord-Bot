5451b01017c3d4fec7bd9e171ced5f9e546cd592
val9212 <val92.hottlet@gmail.com>
2025-04-25 10:04:21 +0200

    Refactor brazzers command for DiscordJS v14

--- commands/brazzers.js
+++ commands/brazzers.js
@@ -1,27 +1,54 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const snek = require('snekfetch');
-const fsn = require('fs-nextra');
+const { Client, PermissionsBitField, AttachmentBuilder } = require('discord.js');
+const { createCanvas, loadImage } = require('canvas'); 
+const fetch = require('node-fetch');
+const fs = require('fs/promises');
+
 exports.run = async (client, message, args) => {
-  if (!message.guild.member(client.user).hasPermission('ATTACH_FILES')) return message.reply('Sorry, i dont have the perms to do this cmd i need ATTACH_FILES. :x:')
-   const { Canvas } = require('canvas-constructor');
-    if (message.mentions.users.size < 1) return message.channel.send("You didn't mention a user to make them beautiful");
-   const getSlapped = async (person) => {
-    const plate = await fsn.readFile('./assets/images/plate_brazzers.png');
-    const png = person.replace('.gif', '.png');
-    const { body } = await snek.get(png);
-    return new Canvas(634, 675)
-    .setColor(0x00A2E8)
-    .addRect(0, 0, 634, 675)
-    .addImage(body, 0, 0, 634, 675)
-    .addImage(plate, 233, 485, 384, 245)
-    .toBuffer();
+  if (!message.guild.members.me.permissions.has(PermissionsBitField.Flags.AttachFiles)) {
+    return message.reply("Sorry, I need the `ATTACH_FILES` permission to run this command. :x:");
   }
-     try {
-    const person = message.mentions.users.first().avatarURL;
-    const result = await getSlapped(person);
-    await message.channel.send({ files: [{ attachment: result, name: 'brazzers.png' }] });
+
+  if (message.mentions.users.size < 1) {
+    return message.channel.send("You didn't mention a user to make them beautiful :eyes:");
+  }
+
+  const getSlapped = async (avatarURL) => {
+    const plate = await fs.readFile('./assets/images/plate_brazzers.png');
+    const avatarURLPng = avatarURL.replace('.gif', '.png') + '?size=512';
+    const response = await fetch(avatarURLPng);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch avatar: ${response.statusText}`);
+    }
+
+    const buffer = await response.arrayBuffer();
+    const avatar = await loadImage(Buffer.from(buffer));
+    const plateImage = await loadImage(plate);
+
+    const canvas = createCanvas(634, 675);
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = '#00A2E8';
+    ctx.fillRect(0, 0, 634, 675);
+    ctx.drawImage(avatar, 0, 0, 634, 675);
+    ctx.drawImage(plateImage, 233, 485, 384, 245);
+
+    return canvas.toBuffer();
+  };
+
+  try {
+    const person = message.mentions.users.first();
+    if (!person || !person.displayAvatarURL()) {
+      return message.reply("I couldn't find the mentioned user's avatar.");
+    }
+
+    const avatarURL = person.displayAvatarURL({ extension: 'png', size: 512 });
+    const result = await getSlapped(avatarURL);
+
+    const attachment = new AttachmentBuilder(result, { name: 'brazzers.png' });
+    await message.channel.send({ files: [attachment] });
   } catch (error) {
-    throw error;
+    console.error(error);
+    message.reply("An error occurred while generating the image. :x: " + error.message);
   }
-}
+};
