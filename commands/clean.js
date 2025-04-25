c4164b20c7630ef406c46fbda2289a6449940d94
val9212 <val92.hottlet@gmail.com>
2025-04-25 09:59:21 +0200

    Refactor clean command to improve readability and error handling

--- commands/clean.js
+++ commands/clean.js
@@ -1,13 +1,26 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-exports.run = (client, message, args) => {
-      if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send("Sorry, you do not have permission to perform the clean command.");
-      let num = (!!args.slice(0).join(' ')) ? parseInt(args.slice(0).join(' ')) || 99 : 99;
-      message.channel.fetchMessages({limit:num}).then(messages => {
-      let ms = messages.filter(m => m.author.id === client.user.id);
-      if (ms.size === 1) { ms.first().delete(); return message.channel.send("**Mister UWU messages have been deleted**") }
-      if (ms.size < 1) return message.channel.send("**No messages found to clean**")
-      message.channel.bulkDelete(ms, true).then(() => message.channel.send("**Mister UWU messages have been deleted**"))
-  })
-}
-   
+const { PermissionsBitField } = require('discord.js');
+
+exports.run = async (client, message, args) => {
+    if (!message.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+        return message.channel.send("Sorry, you do not have permission to perform the clean command.");
+    }
+
+    const num = parseInt(args[0]) || 99;
+
+    try {
+        const messages = await message.channel.messages.fetch({ limit: num });
+        const botMessages = messages.filter(m => m.author.id === client.user.id);
+
+        if (botMessages.size < 1) {
+            return message.channel.send("**No messages found to clean**");
+        }
+
+        await message.channel.bulkDelete(botMessages, true);
+        message.channel.send("**Mister UWU messages have been deleted**").then(msg => {
+            setTimeout(() => msg.delete().catch(() => {}), 5000);
+        });
+    } catch (error) {
+        console.error(error);
+        message.channel.send("An error occurred while trying to delete messages.");
+    }
+};
