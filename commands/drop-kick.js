0a72aaf9e55831112b46807742407f7ba1603a59
val9212 <val92.hottlet@gmail.com>
2025-04-25 17:01:35 +0200

    Refactor drop-kick command for DiscordJS v14

--- commands/drop-kick.js
+++ commands/drop-kick.js
@@ -1,15 +1,14 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const snekfetch = require("snekfetch")
+const { EmbedBuilder } = require('discord.js');
+
 exports.run = async (client, message, args) => {
-      if (message.mentions.users.size < 1) return message.channel.send("you can't drop kick nobody")
-      let user = message.guild.member(message.mentions.users.first());
-            message.channel.send(`${user} You got a drop kick from ${message.author.username}`,{
-                embed: {
-                    image: {
-                        url: "https://vgy.me/04YbOf.gif"
-                    }
-                }
-            })
-}
-   
+  const user = message.mentions.users.first();
+  if (!user) return message.channel.send("You can't drop kick nobody");
+
+  const embed = new EmbedBuilder()
+    .setImage("https://vgy.me/04YbOf.gif");
+
+  await message.channel.send({
+    content: `${user} You got a drop kick from ${message.author.username}`,
+    embeds: [embed]
+  });
+};
