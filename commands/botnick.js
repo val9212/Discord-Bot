f0288a4fdfc9e8078d6c451401e947904492bc96
val9212 <val92.hottlet@gmail.com>
2025-04-25 09:54:58 +0200

    Update botnick command for DiscordJS v14

--- commands/botnick.js
+++ commands/botnick.js
@@ -1,15 +1,29 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+const { EmbedBuilder } = require('discord.js');
+
 exports.run = (client, message, args) => {
-      if (!message.member.hasPermission("MANAGE_NICKNAMES"))  {
-    return message.reply("You do not have permission to change the bot's nickname");
+    if (!message.member.permissions.has("MANAGE_NICKNAMES")) {
+        return message.reply("You do not have permission to change the bot's nickname.");
     } else {
-      let username = args.join(' ');
-      if (username.length < 1) return message.reply('You must supply a name for the client.')
-       message.guild.members.get('618011652448845854').setNickname(username);
-        const embed = new Discord.RichEmbed()
-        .setColor(0x00A2E8)
-        .addField("Bot username set successfully!", username + " is now the nickname for the bot :white_check_mark:");
-        message.reply({embed})
-  }
-}
+        let username = args.join(' ');
+        if (username.length < 1) return message.reply('You must supply a name for the client.');
+
+        const bot = message.guild.members.cache.get('618011652448845854');
+        if (bot) {
+            bot.setNickname(username)
+                .then(() => {
+                    const embed = new EmbedBuilder()
+                        .setColor(0x00A2E8)
+                        .addFields(
+                            { name: "Bot username set successfully!", value: `${username} is now the nickname for the bot :white_check_mark:` }
+                        );
+                    message.reply({ embeds: [embed] });
+                })
+                .catch(err => {
+                    console.error(err);
+                    message.reply("There was an error trying to set the bot's nickname.");
+                });
+        } else {
+            message.reply("Bot not found in the guild.");
+        }
+    }
+};
