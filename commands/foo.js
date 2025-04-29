c5c48464380f6eecbd3bd82c02947024b0e6e3cf
val9212 <val92.hottlet@gmail.com>
2025-04-29 08:18:33 +0200

    Simplify and clean up `foo.js` command structure.

--- commands/foo.js
+++ commands/foo.js
@@ -1,6 +1,3 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
 exports.run = (client, message, args) => {
-      message.reply(" Bar!")
-}
-   
+      message.reply("Bar!");
+    };
