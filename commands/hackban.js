c529a0499443e6dcc0c529d810fab0c586a4e54c
val9212 <val92.hottlet@gmail.com>
2025-04-29 08:20:13 +0200

    Refactor hackban command to use modern Discord.js and better-sqlite3

--- commands/hackban.js
+++ commands/hackban.js
@@ -1,43 +1,67 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const sql = require("sqlite");
-sql.open("./assets/guildsettings.sqlite");
-exports.run = async (client, message, args) => {
-  sql.get(`SELECT * FROM scores WHERE guildId ="${message.guild.id}"`).then(row => {
-  const prefixtouse = row.prefix
-  const usage = new Discord.RichEmbed()
-            .setColor(0x00A2E8)
-            .setThumbnail(client.user.avatarURL)
-            .setTitle("Command: " + prefixtouse + "hackban")
-            .addField("Usage", prefixtouse + "hackban <ID> <reason>")
-            .addField("Example", prefixtouse + "hackban 130515926117253122 self bot that dms server links and left.")
-            .setDescription("Description: " + "Bans a user without needing to be in the server");
+const { Client, EmbedBuilder } = require("discord.js");
+const Database = require('better-sqlite3');
+const db = new Database("./assets/guildsettings.sqlite");
 
-  if (message.member.hasPermission("BAN_MEMBERS")) {
-  if (!message.guild.member(client.user).hasPermission('BAN_MEMBERS')) return message.reply('Sorry, i dont have the perms to do this cmd i need BAN_MEMBERS. :x:')
-  let user = args[0]
-  if (isNaN(user)) return message.channel.send(usage)
-  let reason = args[1] || `Moderator didn't give a reason.`;
-  if (isNaN(user)) return message.channel.send(usage)
-  if (!user) return message.reply('You must supply a User Resolvable, such as a user id.')
-  let guild = message.member.guild;
-  if (user.length < 1) return message.channel.send("need to provide a valid user id to ban them");
-  if (user === message.author.id) return message.channel.send(`:x: Well no you can't hackban yourself`);
-  if (message.guild.members.get(user)) return message.channel.send(`:x: That user is in this server, please use ban instead`);
-  let modlog = message.guild.channels.find(channel => channel.name == row.logschannel);
-  message.guild.ban(user, 2);
-  sql.run(`UPDATE scores SET casenumber = ${row.casenumber + 1} WHERE guildId = ${message.guild.id}`);
-  const embed = new Discord.RichEmbed()
-    .setColor(0x00A2E8)
-    .setTitle("Case #" + row.casenumber + " | Action: Hack Ban")
-    .addField("Moderator", message.author.tag + " (ID: " + message.author.id + ")")
-    .addField("User ID", user)
-    .addField("Reason", reason, true)
-    .setFooter("Time used: " + message.createdAt.toDateString())
-  message.channel.send("ID: " + user + ", has been banned from the server.")
-  if (!modlog) return;
-  client.channels.get(modlog.id).send({embed});
+exports.run = async (client, message, args) => {
+  try {
+    const row = db.prepare("SELECT * FROM scores WHERE guildId = ?").get(message.guild.id);
+    if (!row) {
+      return message.channel.send("No settings found for this guild.");
     }
-  })
-}
-   
+
+    const prefixtouse = row.prefix;
+
+    const usage = new EmbedBuilder()
+      .setColor(0x00A2E8)
+      .setThumbnail(client.user.avatarURL())
+      .setTitle(`Command: ${prefixtouse}hackban`)
+      .addFields(
+        { name: "Usage", value: `${prefixtouse}hackban <ID> <reason>` },
+        { name: "Example", value: `${prefixtouse}hackban 130515926117253122 self bot that dms server links and left.` },
+        { name: "Description", value: "Bans a user without needing to be in the server" }
+      );
+
+    if (!message.member.permissions.has("BAN_MEMBERS")) {
+      return message.channel.send("You do not have permission to use this command.");
+    }
+    if (!message.guild.members.me.permissions.has("BAN_MEMBERS")) {
+      return message.reply("I don't have the required permissions to ban members. :x:");
+    }
+
+    const user = args[0];
+    if (isNaN(user)) return message.channel.send({ embeds: [usage] });
+
+    let reason = args.slice(1).join(" ") || "Moderator didn't give a reason.";
+
+    if (!user) return message.reply("You must provide a user ID.");
+    if (user === message.author.id) return message.channel.send(":x: You cannot hackban yourself.");
+    if (message.guild.members.cache.get(user)) {
+      return message.channel.send(":x: That user is in the server. Please use the regular ban command.");
+    }
+
+    const modlog = message.guild.channels.cache.find(channel => channel.id === row.logschannel);
+
+    message.guild.bans.create(user, { reason, days: 2 });
+    db.prepare("UPDATE scores SET casenumber = ? WHERE guildId = ?").run(row.casenumber + 1, message.guild.id);
+
+    const embed = new EmbedBuilder()
+      .setColor(0x00A2E8)
+      .setTitle(`Case #${row.casenumber} | Action: Hack Ban`)
+      .addFields(
+        { name: "Moderator", value: `${message.author.tag} (ID: ${message.author.id})` },
+        { name: "User ID", value: user },
+        { name: "Reason", value: reason, inline: true }
+      )
+      .setFooter({ text: `Time used: ${message.createdAt.toDateString()}` });
+
+    message.channel.send(`ID: ${user}, has been banned from the server.`);
+
+    if (modlog && row.logsenabled !== "disabled") {
+      modlog.send({ embeds: [embed] });
+    }
+
+  } catch (error) {
+    console.error("Error executing hackban command:", error);
+    message.channel.send("An error occurred while processing the command.");
+  }
+};
