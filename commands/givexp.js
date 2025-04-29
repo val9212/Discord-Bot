327417e5a13a4178744368ac3c4733b6680ecf52
val9212 <val92.hottlet@gmail.com>
2025-04-29 08:19:38 +0200

    Refactor givexp command to use better-sqlite3 and modern Discord.js

--- commands/givexp.js
+++ commands/givexp.js
@@ -1,33 +1,58 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const sql = require("sqlite");
-sql.open("./assets/guildsettings.sqlite");
-exports.run = (client, message, args) => {
-    if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("Need MANAGE_GUILD permission to use this command.")
-    const user = message.mentions.users.first();
-     if (message.mentions.users.size < 1) {
-        const amount = parseInt(args[0]);
-        if (amount < 1) return message.channel.send("Didnt provide any xp to give.");
-        if (amount >= 9999999) return message.channel.send("Max xp to give is 9999999")
-        sql.get(`SELECT * FROM profiles WHERE guildId ="${message.guild.id}" AND userId ="${message.author.id}"`).then(row2 => {
-            if (!row2) message.channel.send("You needs to start talking first.")
-            if (row2.xp >= 9999999) return message.channel.send("Max xp is 9999999")
-               if (isNaN(amount)) return message.channel.send("Not a valid number")
-               sql.run(`UPDATE profiles SET xp = ${row2.xp += amount}, username = "${message.author.username}" WHERE guildId ="${message.guild.id}" AND userId = ${message.author.id}`);
-               message.channel.send("I have given xp to: " + message.author.username + " " + amount + "XP");
-        })
-     } else {
-     const amount = parseInt(args[1]);
-     if (amount < 1) return message.channel.send("Didnt provide any xp to give.");
-     if (amount >= 9999999) return message.channel.send("Max xp to give is 9999999")
-    sql.get(`SELECT * FROM profiles WHERE guildId ="${message.guild.id}" AND userId ="${user.id}"`).then(row => {
-     if (!row) message.channel.send("User needs to start talking first.")
-     if (row.xp >= 9999999) return message.channel.send("Max xp is 9999999")
-        if (amount >= 999999) return message.channel.send("Max xp to give is $999999")
-        if (isNaN(amount)) return message.channel.send("Not a valid number")
-        sql.run(`UPDATE profiles SET xp = ${row.xp += amount} WHERE guildId ="${message.guild.id}" AND userId = ${user.id}`);
-        message.channel.send("I have given xp to: " + user.username + " " + amount + "XP");
-        })
-    }
-}
+const { EmbedBuilder, PermissionsBitField } = require('discord.js');
+const Database = require('better-sqlite3');
+const db = new Database("./assets/guildsettings.sqlite");
 
+exports.run = async (client, message, args) => {
+  if (!message.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
+    return message.reply('You need `MANAGE_GUILD` permission to use this command.');
+  }
+
+  if (message.mentions.users.size < 1) {
+    const amount = parseInt(args[0]);
+
+    if (isNaN(amount) || amount < 1) {
+      return message.reply('You didn\'t provide any valid XP amount to give.');
+    }
+
+    if (amount >= 9999999) {
+      return message.reply('The maximum XP you can give is **9999999**.');
+    }
+
+    const row2 = db.prepare('SELECT * FROM profiles WHERE guildId = ? AND userId = ?').get(message.guild.id, message.author.id);
+    if (!row2) {
+      return message.reply('You need to start interacting in the server first.');
+    }
+
+    if (row2.xp >= 9999999) {
+      return message.reply('You have reached the max XP limit of **9999999**.');
+    }
+
+    db.prepare('UPDATE profiles SET xp = ? WHERE guildId = ? AND userId = ?').run(row2.xp + amount, message.guild.id, message.author.id);
+
+    return message.reply(`I have given **${amount}** XP to **${message.author.username}**.`);
+  } else {
+    const user = message.mentions.users.first();
+    const amount = parseInt(args[1]);
+
+    if (isNaN(amount) || amount < 1) {
+      return message.reply('You didn\'t provide any valid XP amount to give.');
+    }
+
+    if (amount >= 9999999) {
+      return message.reply('The maximum XP you can give is **9999999**.');
+    }
+
+    const row = db.prepare('SELECT * FROM profiles WHERE guildId = ? AND userId = ?').get(message.guild.id, user.id);
+    if (!row) {
+      return message.reply('This user needs to start interacting in the server first.');
+    }
+
+    if (row.xp >= 9999999) {
+      return message.reply('This user has reached the max XP limit of **9999999**.');
+    }
+
+    db.prepare('UPDATE profiles SET xp = ? WHERE guildId = ? AND userId = ?').run(row.xp + amount, message.guild.id, user.id);
+
+    return message.reply(`I have given **${amount}** XP to **${user.username}**.`);
+  }
+};
